3370aef90155f337ba742c68d1cbeb1acc181fc5
Angelo Lirazan <angelo.g.lirazan@gmail.com>
2015-05-14 19:46:48 -0700

    New Deck functionality + form element implemented. Corresponding JS helpers and CSS added

---
+++ app/assets/javascripts/decks-new.js
@@ -0,0 +1,65 @@
+ng_deck_test.controller('ng_c_deck_list', function($scope){
+  $scope.deck = [];
+
+  // initialize card inputs
+  // set default values for selects
+  $scope.card_name = '';
+  $scope.card_quantity = '1';
+  $scope.card_type = 'Monster';
+
+  $scope.plus_card_quantity = function(e){
+    var c_name = $( e.currentTarget ).parent().attr('card_name');
+    var c_index = find_card_index($scope.deck, c_name);
+    if($scope.deck[c_index].quantity < 3){
+      $scope.deck[c_index].quantity++;
+    }
+  };
+  
+  $scope.minus_card_quantity = function(e){
+    var c_name = $( e.currentTarget ).parent().attr('card_name');
+    var c_index = find_card_index($scope.deck, c_name);
+    if($scope.deck[c_index].quantity > 1){
+      $scope.deck[c_index].quantity--;
+    }//else if($scope.deck[c_index].quantity == 1){ $scope.deck.splice(c_index, 1); }
+  };
+  
+  $scope.add_card_to_deck = function(){
+    var card = create_card($scope.card_name, $scope.card_quantity, $scope.card_type);
+    if(card){
+      if(find_card_index($scope.deck, card.name) === false){
+        $scope.deck.push(card);
+      }else{
+        alert('Card already in deck.');
+      }
+    }
+  };
+  
+  $scope.remove_card_from_deck = function(e){
+    var c_name = $( e.currentTarget ).parent().attr('card_name');
+    var remove_i = find_card_index($scope.deck, c_name);
+    $scope.deck.splice(remove_i, 1);
+  };
+  
+  // deck check and add deck meta
+  $scope.deck_check = function(e){
+    // validate form
+    var d_name = $(e.currentTarget).find('input#deck_name').val();
+    if(d_name == ""){
+      alert("Please enter a deck name.");
+      e.preventDefault();
+    }
+    // var d_notes = $(e.currentTarget).find('textarea#deck_notes').val();
+    var deck_count = 0;
+    // check for 40 cards
+    Object.keys($scope.deck).forEach(function(key){
+      deck_count += $scope.deck[key].quantity;
+    });
+    if(deck_count <= 60 && deck_count >= 40){
+      var deck_json = angular.toJson($scope.deck);
+      $('input#f_contents').val(deck_json);
+    }else{
+      alert('Please enter a deck with 40 to 60 cards');
+      e.preventDefault();
+    }
+  };
+});
--- app/assets/javascripts/decks.js
+++ app/assets/javascripts/decks.js
@@ -1,1 +1,27 @@
 //= require angular
+// leave it to action-level JS to initialize:
+//   deck_array
+//   analyze_array
+
+var ng_deck_test = angular.module('ng_app_deck_test', []);
+
+function create_card(card_name, card_quantity, card_type){
+  // to-do: return false on invalid card
+  if(!card_name){
+    alert('Please enter a card name.');
+    return false;
+  }
+  return {
+    'name': card_name,
+    'quantity': parseInt(card_quantity),
+    'type': card_type.toLowerCase()
+  };
+}
+
+function find_card_index(deck_array, card_name){
+  var index = false;
+  for (var i = 0; i < deck_array.length; i++) {
+    if(deck_array[i].name == card_name){ index = i; break; }
+  }
+  return index;
+}
